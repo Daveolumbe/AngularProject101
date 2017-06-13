5d0ca21e30611c2f640e75cb888383fbf96a99a7
David Olumbe <davidolumbe@Development.Quartix.local>
2017-06-13 15:47:36 +0100

    initial commit

---
+++ src/app/models/register.ts
@@ -0,0 +1,7 @@
+export class register {
+    public date: Date;
+    public contactName: string;
+    public contactEmail: string;
+    public contactMessage: string;
+    public contactWebsite: string;
+}
